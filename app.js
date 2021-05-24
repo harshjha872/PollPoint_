dc7a64f75e165ae0fef9569c58902ad77680ee9c
harshjha872 <harsh.1923ec1128@kiet.edu>
2021-05-25 01:07:59 +0530

    choices ,responsiveness and pagination panel left,

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const random = function () {
     return Math.trunc(Math.random() * 5) + 1;
 }
+// const random2 = function () {
+//     return Math.trunc(Math.random() * 255) + 1;
+// }
 const img = document.querySelector('.img');
 
 const btnLeft = document.querySelector('.left')
@@ -8,7 +11,12 @@ const btnRight = document.querySelector('.right')
 const images = document.querySelectorAll('.imagesss');
 const section1 = document.querySelector('.section--0');
 const header = document.querySelector('.header');
-const mainImage = document.querySelector('.mainImage')
+const mainImage = document.querySelector('.mainImage');
+const sections = document.querySelectorAll('.section');
+const home = document.querySelector('.home');
+home.addEventListener('click', function () {
+    mainImage.scrollIntoView({ behavior: 'smooth' })
+});
 let current = 0;
 let maxslide = images.length
 const nxt = function () {
@@ -39,6 +47,26 @@ setInterval(nxt, 4000)
 btnRight.addEventListener('click', nxt)
 btnLeft.addEventListener('click', Pre)
 
+// sections.forEach(function (El) {
+
+// })
+
+const Observer = new IntersectionObserver(function (entries, Observer) {
+    const [entry] = entries;
+    if (entry.isIntersecting === false) return;
+
+    entry.target.classList.remove('section--hidden');
+    Observer.unobserve(entry.target);
+}, {
+    root: null,
+    threshold: 0.15
+})
+sections.forEach((El) => {
+    Observer.observe(El);
+});
+// setInterval(function () {
+//     document.querySelector('.pollpoint__main').style.color = `rgb(${random2()}, ${random2()}, ${random2()})`
+// }, 80)
 // const obeerserver = new IntersectionObserver(function (entries, obeerserver) {
 //     // entry
 //     const [entry] = entries;
